bab59128e29bc6c785a3f22e00259dc5393665d6
NeemaMwende <gracepete12@gmail.com>
2024-07-13 23:12:45 +0300

    parsing request paths

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 var http = require('http');
 const PORT = 3000;
+var url = require('url');
+
 var server = http.createServer(function(req, res){ 
+    
+    var parsedUrl = url.parse(req.url, true);
+
+    var path = parsedUrl.pathname;
+    var trimmedpath = path.replace(/^\/+|\/+$/g, '');
+
+
     res.end('Hello World\n');
+
+    console.log('Request is received on path ' +trimmedpath);
+
+
 });
 
 server.listen(PORT, function(){
