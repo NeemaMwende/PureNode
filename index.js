a8363aad6f63fadb9edc2264a8d29dcb76941a27
NeemaMwende <gracepete12@gmail.com>
2024-07-22 18:04:45 +0300

    query string object

--- index.js
+++ index.js
@@ -5,18 +5,20 @@ var url = require('url');
 var server = http.createServer(function(req, res){ 
     
     var parsedUrl = url.parse(req.url, true);
-
     var path = parsedUrl.pathname;
     var trimmedpath = path.replace(/^\/+|\/+$/g, '');
 
+    //get the query string as an object
+    var queryStringObject = parsedUrl.query;
 
+    // Get HTTP method
+    var method = req.method.toLowerCase();
     res.end('Hello World\n');
-
-    console.log('Request is received on path ' +trimmedpath);
-
-
+    console.log('Request received on path: ' + trimmedPath + 
+        ' with method: ' + method + 
+        ' and with these query string parameters: ', queryStringObject);
 });
 
 server.listen(PORT, function(){
-    console.log(`The server is listening to port ${PORT}`);
-})
+    console.log(`The server is listening on port ${PORT}`);
+});
